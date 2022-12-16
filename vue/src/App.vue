96ea19df8962103235faac4deff2adc4c9a4dd13
Andy Glover <andy.glover@amd.com>
2022-12-15 17:37:47 -0800

    Initial commit.

---
+++ vue/src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { ref } from 'vue';
+import { helperNameMap } from '@vue/compiler-core';
+
+const merpRss = 'https://tech.lgbt/@solarmerps.rss';
+const shitPosts = ref({});
+const soffImage = ref('');
+
+onBeforeMount(async () => {
+  const superGay = await fetch(merpRss).then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"));
+  if (superGay.querySelector('image') && xmldoc.querySelector('image').querySelector('url')) {
+    soffImage.value = xmldoc.querySelector('image').querySelector('url').textContent;
+  }
+  
+  shitPosts.value = superGay.querySelectorAll('item').map(gayPost => {
+      const tempNode = document.createElement('div');
+      tempNode.innerHTML = gayPost.querySelector('description').textContent;
+      return tempNode.textContent;
+    });
+    return {shitPosts};
+});
+
+const messages = {
+  scanning: [
+    'Scanning for signs of gay'
+  ],
+  yes: [
+    'Gay detected. Continuing scan.',
+    'Large amounts of gay detected. Continuing scan.',
+    'Massive amounts of gay detected. Continuing scan. For science!',
+    'Excessive amounts of gay detected. Scan aborted.'
+  ],
+  no: [
+    'Seeking signs of gay.',
+    'No gay detected. Yet. Further research necessary.',
+    'Still no gay. Having doubts about initial hypothesis.',
+    "Hang on. Something isn't right here. Sample contains no gay. "
+  ],
+  paradox: [
+    'Paradox detected. This statement is a lie.'
+  ]
+}
+
+</script>
+
+<template>
+  <div :style="{ backgroundImage: `url('${soffImage}')` }" class="container">
+    <section class="row row-col-3" v-for="gayPost in shitPosts">
+      <div class="card">{{ gayPost }}</div>
+    </section>
+  </div>
+</template>   
+
+<style scoped>
+
+</style>
---
+++ vue/src/main.js
@@ -0,0 +1,5 @@
+import '/src/scss/styles.scss';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
